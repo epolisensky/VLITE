bf38c65759b47b99d7c62e833b8263cfb587112b
Emily Richards <e.e.richards89@gmail.com>
2018-04-10 10:41:53 -0400

    rtd autodoc attempt 8

--- vdp/docs/source/conf.py
+++ vdp/docs/source/conf.py
@@ -21,7 +21,7 @@ import sys
 import mock
 sys.path.insert(0, os.path.abspath('../..'))
 
-MOCK_MODULES = ['numpy', 'pandas', 'psycopg2']
+MOCK_MODULES = ['numpy', 'pandas', 'psycopg2', 'astropy']
 for mod_name in MOCK_MODULES:
     sys.modules[mod_name] = mock.Mock()
 
